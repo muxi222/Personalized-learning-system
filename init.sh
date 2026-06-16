ecdec4429a9a748e3396bac814dd2f2ca09ce491
tony <zhangxuhua@zgci.ac.cn>
2026-06-16 07:56:22 +0000

    update config

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo apt-get install fonts-wqy-microhei fonts-wqy-zenhei
 sudo apt-get install sqlite3
 sudo apt-get install nvtop
 #ssh-keygen -m PEM -t rsa -b 4096
-cp ssh/* ~/.ssh/
+#cp ssh/* ~/.ssh/
 
-cp tmux/tmux.conf ~/.tmux.conf
+#cp tmux/tmux.conf ~/.tmux.conf
